8680be7598ab9e9c40b4b3a14a2b2ba26ec941c2
ferchosorio <90169280+ferchosorio@users.noreply.github.com>
2021-09-17 15:21:36 -0500

    Agrego columnas de usuario y contraseña

--- controlador.py
+++ controlador.py
@@ -1,12 +1,12 @@
-from db import connection()
+from db import conectar
 #En este archivo se ejecutan las acciones que interactuan con la base de datos
 
-def insertar_administracion(nombre,identificacion,telefono,email):
-    conexion = connection()
-    with conexion.cursor as cursor:
-        cursor.execute("INSERT INTO administration(nombre,identificacion,telefono,email) VALUES (%s %s %s %s)",(nombre,identificacion,telefono,email))
+def insertar_administracion(nom,idn,telf,mail,usu,cont):
+    conexion = conectar()
+    with conexion.cursor() as cursor:
+        cursor.execute("INSERT INTO administration(nombre,identificacion,telefono,email,usuario,contrasena) VALUES (%s, %s, %s, %s, %s, %s)",(nom,idn,telf,mail,usu,cont))
+        conexion.commit()
         conexion.close()
 #Esta función permite insertar los datos adquiridos mediante POST a la base de datos en la tabla administration
 
 #Es solo un ejemplo para tener en cuenta, aún que así funciona.
-
